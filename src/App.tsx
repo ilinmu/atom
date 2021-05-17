d3312db023c90c55bbf4b6d480731a1f9bc16675
ilinmu <ilinmu@gmail.com>
2021-05-17 22:27:33 +0800

    feat(/button): add Button component
    add Button component and passed the test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
-    <div>
-      Hello Atom
+    <div className="App">
+      <Button
+        type={ButtonType.Primary}
+        size={ButtonSize.Large}
+        disabled
+      > 
+        Hello
+      </Button>
+      <Button
+        type={ButtonType.Danger}
+        size={ButtonSize.Small}
+        autoFocus
+      > 
+        Hello
+      </Button>
+      <Button
+        type={ButtonType.Link}
+        size={ButtonSize.Small}
+        target="_blank"
+        href="http://www.qq.com"
+      > 
+        QQ LINK
+      </Button>
+      <Button
+        type={ButtonType.Link}
+        size={ButtonSize.Small}
+        href="http://www.qq.com"
+        disabled
+      > 
+        QQ LINK
+      </Button>
     </div>
   );
 }
